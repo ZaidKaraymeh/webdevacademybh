980d470ef5f36d4bd1289281b71367c6fbdf4b20
Zaid <karaymehzaid@gmail.com>
2022-05-06 17:04:44 +0300

    0.1.2 Deploy

--- backend/settings.py
+++ backend/settings.py
@@ -141,9 +141,9 @@ MEDIA_URL = '/media/'
 import os
 import psycopg2
 
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
