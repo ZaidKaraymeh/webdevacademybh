0c91b99f01219c477aef5593f17ef69507dd08bc
Zaid <karaymehzaid@gmail.com>
2022-05-08 13:23:18 +0300

    0.1.2 Week 1-2, Mobile Responsive

--- backend/settings.py
+++ backend/settings.py
@@ -151,5 +151,4 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 if os.getcwd() == '/app':
     # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # SECURE_SSL_REDIRECT = True
-    # DEBUG = False
-    pass
+    DEBUG = False
